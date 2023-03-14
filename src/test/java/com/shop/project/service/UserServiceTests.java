a0aff3cd521d546e6ddb1af8672c0fbd62fbf53e
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2023-03-14 01:26:23 +0100

    Intro to unit testing

---
+++ src/test/java/com/shop/project/service/UserServiceTests.java
@@ -0,0 +1,16 @@
+package com.shop.project.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceTests {
+
+	@Test
+	void testGetByUserName() {
+		fail("Not yet implemented");
+	}
+
+}
